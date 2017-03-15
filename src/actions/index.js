94932906d4c32c15c3f73e35a857329e834bf2d3
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-15 22:07:43 +0000

    Query string created for AJAX call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const OPEN_WEATHER_API_KEY = '9b1e26b7dac3c0f0fec461c2d3d01883';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${OPEN_WEATHER_API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},es`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
