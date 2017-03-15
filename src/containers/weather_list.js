1d0f088e2d48c44611e74fc746e9f71893219058
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-15 23:55:29 +0000

    mapping the temperatures nested in the data into a flat array

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 class WeatherList extends Component {
   renderWeather(cityData) {
     const city = cityData.city;
+    const temps = cityData.list.map(weather => weather.main.temp);
 
     return(
       <tr key={city.id}>
