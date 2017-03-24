4a05d5b3ccc2c5ab382438d92f1d31a4cedb07d2
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-24 01:52:30 +0000

    Revert "Revert "using HTTPS""
    This reverts commit 7a4b413d1c11d789035947a0b8a807e482a18f9e.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const OPEN_WEATHER_API_KEY = '9b1e26b7dac3c0f0fec461c2d3d01883';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${OPEN_WEATHER_API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${OPEN_WEATHER_API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
